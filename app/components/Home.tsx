31d71e6c3ab741536375d925da49cb29e99614bc
Chris Hale <chris.hale@me.com>
2019-10-26 15:58:05 -0600

    Added Handlebars

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -8,6 +8,7 @@ import axios from 'axios';
 import * as path from 'path';
 import * as fs from 'fs';
 import * as Handlebars from 'handlebars';
+import Button from '@material-ui/core/Button';
 
 type Props = {};
 const initialState = { test: '' };
@@ -29,7 +30,9 @@ export default class Home extends Component<Props, State> {
 
     const template = Handlebars.compile(data.toString());
     console.log(template);
-    const context = { title: 'My New Post', body: 'This is my first post!' };
+    const context = {
+      models: [{ name: 'test', type: 'string', required: 'true' }]
+    };
     const html = template(context);
     console.log(html);
   }
@@ -39,6 +42,9 @@ export default class Home extends Component<Props, State> {
       <div className={styles.container} data-tid="container">
         <h2>Home</h2>
         <div>test response {this.state.test}</div>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
         <Link to={routes.COUNTER}>to Counter</Link>
       </div>
     );
